93e0b82683c83c13b4e26e32d969f046a8d2c750
Mahin <mahin12112006@gmail.com>
2026-02-05 23:24:10 +0530

    Created playground.js
    Created playground.js file for testing basic functionalities.

---
+++ backend/playground.js
@@ -0,0 +1,11 @@
+// playground.js
+// Contributor: Mahin
+// Test file for backend practice and collaboration. This is not part of the deployed website.
+
+console.log("Hello from the backend playground!");
+
+function simpleGreet(name) {
+    return Hi, ${name}. This is a test backend function.;
+}
+
+console.log(simpleGreet("Paradigm Shift Team"));
