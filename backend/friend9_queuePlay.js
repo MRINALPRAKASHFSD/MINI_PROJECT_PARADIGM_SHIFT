bbcebe1de346b49f815989d74b885e2affc7ccdd
Prarock83 <prathamsharmarocks@gmail.com>
2026-02-05 21:45:47 +0530

    Add TaskQueue class with task execution functionality
    Implement a simple task queue in JavaScript for backend practice.

---
+++ backend/friend9_queuePlay.js
@@ -0,0 +1,23 @@
+// friend9_queuePlay.js
+// Author: Vishal Kapoor
+// Illustrates a simple task queue in plain JS. Purely for backend practice.
+
+class TaskQueue {
+    constructor() {
+        this.tasks = [];
+    }
+    add(task) {
+        this.tasks.push(task);
+    }
+    runAll() {
+        this.tasks.forEach((task, i) => {
+            console.log(⁠ Running task #${i + 1}: ⁠);
+            task();
+        });
+    }
+}
+
+const queue = new TaskQueue();
+queue.add(() => console.log("Send notification"));
+queue.add(() => console.log("Process payroll"));
+queue.runAll();
